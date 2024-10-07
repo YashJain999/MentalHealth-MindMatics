daea97dd4ee7f4410287dd3ba3187acae14b1725
swapniljoshi123 <141126193+swapniljoshi123@users.noreply.github.com>
2024-10-07 15:54:47 +0530

    Your commit message here

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Landing from "./pages/Landing";
 import Login from './pages/Login';
 import Home from "./pages/Home";
+import Questionnaire from "./pages/Questionnnaire";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/questionnaire" element={<Home/>}/>
       </Routes>
     </div>
   );
